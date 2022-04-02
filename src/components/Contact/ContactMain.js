e2ea4dc3d54add84501d1fa2322420b03283aba6
Kacper <kacpermak24@gmail.com>
2022-04-03 00:45:26 +0100

    contact form update
    Rename JS Components - capitalise

--- src/components/Contact/ContactMain.js
+++ src/components/Contact/ContactMain.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { styled } from "@mui/material/styles";
 
 import emailjs from "@emailjs/browser";
 
@@ -10,20 +9,15 @@ const template_id = "template_P4pMVKRx";
 const user_id = "user_GbF3UtpnVkEe6unAsSWg3";
 
 function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const [nameError, setNameError] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [messageError, setMessageError] = useState("");
-
   const [open, setOpen] = useState(false);
 
   const [values, setValues] = React.useState({
     message: "",
     name: "",
     email: "",
+    messageError: "",
+    nameError: "",
+    emailError: "",
   });
 
   const handleChange = (prop) => (event) => {
@@ -44,42 +38,44 @@ function Contact() {
     let error = false;
 
     const params = {
-      from_name: name,
-      from_email: email,
-      message_html: message,
+      from_name: values.name,
+      from_email: values.email,
+      message_html: values.message,
     };
 
-    if (name.length < 3 || /\d/.test(name) === true) {
+    if (values.name.length < 3 || /\d/.test(values.name) === true) {
       error = true;
-      setNameError("At least two characters");
+      setValues({ ...values, nameError: "At least two characters" });
     } else {
-      setNameError(" ");
+      setValues({ ...values, nameError: "" });
     }
 
     if (
-      email.length <= 0 ||
-      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email) !== true
+      values.email.length <= 0 ||
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(values.email) !== true
     ) {
       error = true;
-      setEmailError("Incorrect email");
+      setValues({ ...values, emailError: "Incorrect email" });
     } else {
-      setEmailError("");
+      setValues({ ...values, emailError: "" });
     }
 
-    if (message.length < 10) {
+    if (values.message.length < 10) {
       error = true;
-      setMessageError("Message is too short");
+      setValues({ ...values, messageError: "Message is too short" });
     } else {
-      setMessageError("");
+      setValues({ ...values, messageError: "" });
     }
 
     if (error !== true) {
-      emailjs.send(service_id, template_id, params, user_id);
-      setName("");
-      setEmail("");
-      setMessage("");
-      setOpen(true);
+      //emailjs.send(service_id, template_id, params, user_id);
+      // setValues({ ...values, nameError: "" });
+      // setValues({ ...values, emailError: "" });
+      // setValues({ ...values, messageError: "" });
+      // setOpen(true);
     }
+
+    console.log(values);
   };
 
   return (
@@ -93,13 +89,13 @@ function Contact() {
             }}
             noValidate
             autoComplete="off"
+            onSubmit={submit}
           >
             <TextField
               id="outlined-multiline-static"
               label="Your message"
               multiline
               rows={10}
-              defaultValue=""
               fullWidth
               value={values.message}
               onChange={handleChange("message")}
@@ -109,7 +105,6 @@ function Contact() {
               id="outlined-multiline-static"
               label="Your name"
               fullWidth
-              defaultValue=""
               value={values.name}
               onChange={handleChange("name")}
               style={{ marginBottom: 25 }}
@@ -118,17 +113,12 @@ function Contact() {
               fullWidth
               id="outlined-multiline-static"
               label="Your email"
-              defaultValue=""
               value={values.email}
               onChange={handleChange("email")}
               style={{ marginBottom: 30 }}
             />
 
-            <button
-              type="submit"
-              class="button primary-button alt"
-              onclick={submit}
-            >
+            <button type="submit" className="button primary-button alt">
               Send email
             </button>
           </Box>
